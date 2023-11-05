f728d76d1180a69f1f1e99bc5ab6b1571f8c3100
GabrielTrigueiro <gabrieltrigueirofernandes@gmail.com>
2023-11-05 05:09:24 -0300

    setando a página inicial

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,4 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
-
-  # Defines the root path route ("/")
-  # root "posts#index"
-  # ruby bin\rails generate controller Articles index --skip-routes
+  root "articles#index" # Defines essa url como home page
   get "/articles", to: "articles#index"
 end
