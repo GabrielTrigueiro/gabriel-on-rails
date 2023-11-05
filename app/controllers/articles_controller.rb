2c9896fb199e95fc7e9e36c68f900d9e3498660a
GabrielTrigueiro <gabrieltrigueirofernandes@gmail.com>
2023-11-05 06:38:11 -0300

    adicionando rota especifica de artigo

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,3 @@
-# Não é necessário importar nada pois todas as classes esão disponiveis em todos os lugares
-# MVC (Model View Controller) - Design Pattern que divide a responsa da aplicação para ficar mais fácil de entender
 class ArticlesController < ApplicationController
   def index
     @articles = Article.all # fetch all articles
