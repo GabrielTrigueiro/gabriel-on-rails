cde76c391e39f0bcb87d6607b8d1759d6f9f1fff
GabrielTrigueiro <gabrieltrigueirofernandes@gmail.com>
2023-11-05 05:29:46 -0300

    criando modelo para artigo

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,3 +1,5 @@
+# Não é necessário importar nada pois todas as classes esão disponiveis em todos os lugares
+# MVC (Model View Controller) - Design Pattern que divide a responsa da aplicação para ficar mais fácil de entender
 class ArticlesController < ApplicationController
   def index
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  # bin/rails generate controller Articles index --skip-routes
   root "articles#index" # Defines essa url como home page
   get "/articles", to: "articles#index"
 end
